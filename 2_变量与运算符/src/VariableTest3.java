fcc61e3076dd363c995ef69a6356745f3b284bd9
AnXin-HITSZ <anxin.hitsz@outlook.com>
2025-09-19 21:57:31 +0800

    Notes - 20250919

---
+++ 2_变量与运算符/src/VariableTest3.java
@@ -0,0 +1,12 @@
+/*
+测试基本数据类型变量间的运算规则
+
+此 VariableTest3.java 用来测试自动类型提升
+*/
+class VariableTest3 {
+    public static void main(String[] args) {
+        byte b1 = 1;
+        byte b2 = 2;
+        // byte b3 = b1 + b2;
+    }
+}
---
+++ 2_变量与运算符/src/VariableTest4.java
@@ -0,0 +1,10 @@
+/*
+此 VariableTest4.java 用来测试强制类型转换
+*/
+public class VariableTest4 {
+    public static void main(String[] args) {
+        double d = 1.2;
+        int num = (int)d;
+        System.out.println(num);
+    }
+}
