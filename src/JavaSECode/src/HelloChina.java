536b8dfa39c1c5553cb2337a0a8206e3ed3f6f75
AnXin-HITSZ <anxin.hitsz@outlook.com>
2025-10-17 12:29:35 +0800

    Notes - 20251017

---
+++ src/JavaSECode/src/HelloChina.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: HelloChina
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author AnXin
+ * @Create 2025/10/17 11:57
+ * @Version 1.0
+ */
+public class HelloChina {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
