875b2d73007bc85f5dde2cfbb329f4f6cf308181
AnXin-HITSZ <anxin.hitsz@outlook.com>
2025-10-21 21:18:33 +0800

    Notes - 20251021

--- src/JavaSECode/chapter05_array/src/com/anxin_hitsz_01/one/OneArrayTest.java
+++ src/JavaSECode/chapter05_array/src/com/anxin_hitsz_01/one/OneArrayTest.java
@@ -10,4 +10,23 @@ package com.anxin_hitsz_01.one;
  * @Version 1.0
  */
 public class OneArrayTest {
+    public static void main(String[] args) {
+        double[] prices;
+        prices = new double[]{20.32, 43.21, 43.22};
+
+        String[] foods = new String[4];
+        foods[0] = "拌海蜇";
+        foods[1] = "龙须菜";
+        foods[2] = "炝冬笋";
+        foods[3] = "玉兰片";
+
+        // 4. 如何遍历数组？
+        for (int i = 0; i < foods.length; i++) {
+            System.out.println(foods[i]);
+        }
+
+        for (int i = 0; i < prices.length; i++) {
+            System.out.println(prices[i]);
+        }
+    }
 }
--- src/JavaSECode/chapter05_array/src/com/anxin_hitsz_01/one/OneArrayTest1.java
+++ src/JavaSECode/chapter05_array/src/com/anxin_hitsz_01/one/OneArrayTest1.java
@@ -10,4 +10,56 @@ package com.anxin_hitsz_01.one;
  * @Version 1.0
  */
 public class OneArrayTest1 {
+    public static void main(String[] args) {
+        // 5. 数组元素的默认初始化值
+
+        // 整型数组元素的默认初始化值：0
+        int[] arr1 = new int[3];
+        System.out.println(arr1[0]);
+
+        short[] arr2 = new short[4];
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+
+        // 浮点型数组元素的默认初始化值：0.0
+        double[] arr3 = new double[5];
+        System.out.println(arr3[0]);
+
+        // 字符型数组元素的默认初始化值：0（或理解为 '\u0000'）
+        char[] arr4 = new char[4];
+        System.out.println(arr4[0]);
+
+        if (arr4[0] == 0) {
+            System.out.println("1111");
+        }
+
+        if (arr4[0] == '0') {
+            System.out.println("2222");
+        }
+
+        if (arr4[0] == '\u0000') {
+            System.out.println("3333");
+        }
+
+        System.out.println(arr4[0] + 1);
+
+        // boolean 型数组元素的默认初始化值：false
+        boolean[] ar5 = new boolean[4];
+        System.out.println(ar5[0]);
+
+        // 引用数据类型数组元素的默认初始化值：null
+        String[] arr6 = new String[5];
+        for (int i = 0; i < arr6.length; i++) {
+            System.out.println(arr6[i]);
+        }
+
+        if (arr6[0] == null) {
+            System.out.println("aaaa");
+        }
+
+        if (arr6[0] == "null") {
+            System.out.println("bbbb");
+        }
+    }
 }
